946f6c1b28ef1be2db85e22cbfdaa44df4649f35
Ptalabrian <jumabrian24@gmail.com>
2023-08-11 16:13:42 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (End of program)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
+}
