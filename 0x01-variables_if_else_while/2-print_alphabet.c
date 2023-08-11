b2ecb6744c7011919a5eb30632b35e8530a63ecc
Ptalabrian <jumabrian24@gmail.com>
2023-08-11 15:26:35 +0000

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (End of program)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
