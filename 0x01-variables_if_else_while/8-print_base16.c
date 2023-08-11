db8980b96313714f7bead1fbf05c23857adbfa1a
Ptalabrian <jumabrian24@gmail.com>
2023-08-11 16:32:22 +0000

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (End of program)
+ */
+
+int main(void)
+{
+	char i;
+	char j;
+
+	i = 'a';
+	j = 0;
+	while
+		(j < 10) {
+			putchar(j + '0');
+			j++;
+		}
+	while
+		(i <= 'f') {
+			putchar(i);
+			i++;
+		}
+	putchar('\n');
+	return (0);
+}
