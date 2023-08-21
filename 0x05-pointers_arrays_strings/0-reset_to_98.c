d3c0198fa7011ffe663c8d6523582cdfbba9abfd
Ptalabrian <jumabrian24@gmail.com>
2023-08-21 08:51:55 +0000

    0. 98 Battery st. Task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Takes pointer to integer and update its value to 98.
+ * @n: An integer input pointer
+ * Return: 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+
+}
